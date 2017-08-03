0f64f99fcb79b44f9ac10ebab24ea8f50e3e165f
smolaei <molaei.s@gmail.com>
2017-08-03 16:14:54 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('i am the best');
+  res.send('awesome new project');
 });
 
 const PORT = process.env.PORT || 5000;
